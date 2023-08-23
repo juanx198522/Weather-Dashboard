c500dd70787ced523fdb52ce7a281e8427db9d98
juanx198522 <juanx1985@icloud.com>
2023-08-22 20:24:14 -0400

    Problem trying to fetch the 5 days forecast resolved

--- Assets/script.js
+++ Assets/script.js
@@ -55,25 +55,41 @@ fetchButton.addEventListener('click', function () {
 });
 
 searchHistory.addEventListener('click', function (event) {
+  console.log("inside the history click function")
   if (event.target.classList.contains('historyBtn')) {
+    console.log("inside if")
     var nameCity = event.target.textContent;
+    console.log("nameCity: ", nameCity)
      getApi(nameCity);
+     fetchFiveDayForecast(nameCity);  
   }
 });
 
 // Function to fetch 5-day forecast data from OpenWeatherMap API
 function fetchFiveDayForecast(city) {
-
   fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=8588518f199fbc50237600df408b946e&units=metric")
     .then((response) => response.json())
     .then((data) => {
+
       // Loop through the 5-day forecast and update the HTML
       for (let i = 0; i < 5; i++) {
         const forecast = data.list[i * 8]; // Data for every 8th day
 
         // Update HTML elements with forecast data
-        document.getElementById(`cityday${i + 1}`).textContent = forecast.dt_txt;
-        document.getElementById(`cityname${i + 1}`).textContent = data.city.name;
+        const rawDate = forecast.dt_txt;
+        const dateParts = rawDate.split(" ")[0].split("-"); // Split date part
+        
+        // Parse year, month, and day
+        const year = dateParts[0];
+        const month = dateParts[1];
+        const day = dateParts[2];
+        
+        // Create a formatted date string
+        const formattedDate = `${month.padStart(2, '0')}/${day.padStart(2, '0')}/${year}`;
+        
+        // Set the formatted date in the element
+        document.getElementById(`cityday${i + 1}`).textContent = formattedDate;
+                document.getElementById(`cityname${i + 1}`).textContent = forecast.name;
         document.getElementById(`weather-icon${i + 1}`).src = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`;
         document.getElementById(`temp${i + 1}`).textContent = `Temperature: ${forecast.main.temp}°C`;
         document.getElementById(`humid${i + 1}`).textContent = `Humidity: ${forecast.main.humidity}%`;
