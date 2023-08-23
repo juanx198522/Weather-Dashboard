22bc4aa2724e96e0569d7a70b14ed5dd499a6a3f
juanx198522 <juanx1985@icloud.com>
2023-08-23 00:59:26 -0400

    Some visual changes made

--- Assets/script.js
+++ Assets/script.js
@@ -12,7 +12,21 @@ var formatDay = date.toLocaleDateString("en-US");
 var listOfCities = JSON.parse(localStorage.getItem("historyStorage")) || [];
 var historyBtn = document.querySelector(".historyBtn")
 
+// Default city
+var defaultCity = "Miami";
+
+// Function to fetch and display data for the default city
+function displayDefaultCityWeather() {
+  getApi(defaultCity);
+  fetchFiveDayForecast(defaultCity);
+}
+
+// Call the function when the page loads
+window.onload = displayDefaultCityWeather;
+
+
 function getSearchHistory() {
+  searchHistory.innerHTML=""; 
   for (let i = 0; i < listOfCities.length; i++) {
     var historyItem = document.createElement("button");
     historyItem.textContent = listOfCities[i];
@@ -36,23 +50,31 @@ function getApi(city) {
       iconElement.src = iconUrl;
       cityName.textContent = data.name + " (" + formatDay + ")";
       cityName.appendChild(iconElement);
-      var faherenheit = Math.floor(((data.main.temp - 273.15) * 1.8) + 32);
-      tempActual.textContent = "Temp: " + faherenheit + "°F";
+      var celsius = Math.round(data.main.temp - 273.15);
+      tempActual.textContent = "Temp: " + celsius + "°C";
       var windSpeedMph = data.wind.speed * 2.23694;
       var formattedSpeed = windSpeedMph.toFixed(2);
       windActual.textContent = "Wind: " + formattedSpeed + " MPH";
       humidityActual.textContent = "Humidity: " + data.main.humidity + "%";
       var historyEl = cityInput.value;
-      listOfCities.push(historyEl)
-      localStorage.setItem("historyStorage", JSON.stringify(listOfCities));
-      var historyItem = document.createElement("button");
-      historyItem.textContent = historyEl;
-      searchHistory.append(historyItem);
+      
+      console.log(listOfCities);
+
+      if (!listOfCities.includes(historyEl)){
+        listOfCities.push(historyEl)
+        localStorage.setItem("historyStorage", JSON.stringify(listOfCities));
+        // var historyItem = document.createElement("button");
+        // historyItem.textContent = historyEl;
+        // historyItem.classList.add("historyBtn")
+        
+        // searchHistory.append(historyItem);
+
+        getSearchHistory();
+      }
+      
+
     });
 }
-fetchButton.addEventListener('click', function () {
-  getApi(cityInput.value)
-});
 
 searchHistory.addEventListener('click', function (event) {
   console.log("inside the history click function")
@@ -91,9 +113,9 @@ function fetchFiveDayForecast(city) {
         document.getElementById(`cityday${i + 1}`).textContent = formattedDate;
                 document.getElementById(`cityname${i + 1}`).textContent = forecast.name;
         document.getElementById(`weather-icon${i + 1}`).src = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`;
-        document.getElementById(`temp${i + 1}`).textContent = `Temperature: ${forecast.main.temp}°C`;
+        document.getElementById(`temp${i + 1}`).textContent = `Temp: ${forecast.main.temp}°C`;
         document.getElementById(`humid${i + 1}`).textContent = `Humidity: ${forecast.main.humidity}%`;
-        document.getElementById(`wind${i + 1}`).textContent = `Wind: ${forecast.wind.speed} mph`;
+        document.getElementById(`wind${i + 1}`).textContent = `Wind: ${forecast.wind.speed} MPH`;
       }
     })
     .catch((error) => {
@@ -102,7 +124,7 @@ function fetchFiveDayForecast(city) {
 }
 
 // Function to handle button click
-document.getElementById("fetch-button").addEventListener("click", function () {
+fetchButton.addEventListener("click", function () {
   const city = document.getElementById("cityInput").value;
   getApi(city);
   fetchFiveDayForecast(city);
